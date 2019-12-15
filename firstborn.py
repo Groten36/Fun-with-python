ea97cbaa622eea0dee897d28df466b716df7ab69
Magda Ciezka <polhrub@interia.pl>
2019-12-15 03:55:58 +0100

    firstborn.py

--- firstborn.py
+++ firstborn.py
@@ -1,7 +1,11 @@
 from turtle import *
 shape('turtle')
-def square():
+def square(size):
     for i in range(4):
-        forward(100)
+        forward(size)
         right(90)
-square()
+#for i in range(60):
+ #       square(i)
+ #       right(5)
+square(50)
+square(80)
