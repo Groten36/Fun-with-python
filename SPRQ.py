278605ab4fe688257d9515c34e7147cea885eb99
Magda Ciezka <polhrub@interia.pl>
2019-12-16 19:06:45 +0100

    SPRQ.py

---
+++ SPRQ.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+def s_letter():
+        left(90)
+        for i in range(3):
+                left(90)
+                forward(100)
+        
+        for i in range(2):
+                right(90)
+                forward(100)
+
+def p_letter():
+def q_letter():
+def r_letter():
+s_letter()
